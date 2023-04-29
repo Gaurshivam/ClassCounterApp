0a90463a551b1767f9f61015dd70f69d2e84c992
GaurShivam <80668477+Gaurshivam@users.noreply.github.com>
2023-04-29 19:23:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ClassComp from './ClassComponent/ClassComp';
+
+function App() {
+  return (
+    <div className="App">
+    <ClassComp name="Geekster"/>
+    </div>
+  );
+}
+
+export default App;
